9b16f10c149a96ab96c45ab6845239870417ecdf
InteractionEngineer <accounts@jona.email>
2024-04-12 19:46:43 +0200

    First try on implementing OSC functionalities - needs to be transformed into a proper class

--- OSC.ino
+++ OSC.ino
@@ -1,9 +1,71 @@
-void setup() {
-  // put your setup code here, to run once:
+// TODO: needs to be transformed to a proper class
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+#include <OSCData.h>
+
+char ssid[] = "*****************";          // your network SSID (name)
+char pass[] = "*******";                    // your network password
+
+WiFiUDP Udp;                                // A UDP instance to let us send and receive packets over UDP
+const IPAddress outIp(10,40,10,105);        // remote IP of your computer
+const unsigned int outPort = 9999;          // remote port to receive OSC
+const unsigned int localPort = 8888;        // local port to listen for OSC packets (actually not used for sending)
+
+OSCErrorCode error;
+
+void setupOSC() {
+  Serial.begin(115200);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(localPort);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void receiveOSC() {
+  OSCMessage msg;
+  int size = Udp.parsePacket();
 
+  if (size > 0) {
+    while (size--) {
+      msg.fill(Udp.read());
+    }
+    if (!msg.hasError()) {
+      msg.dispatch("/led", led);
+    } else {
+      error = msg.getError();
+      Serial.print("error: ");
+      Serial.println(error);
+    }
+  }
+}
+
+void sendOSC() {
+    OSCMessage msg("/test");
+    msg.add("hello, osc.");
+    Udp.beginPacket(outIp, outPort);
+    msg.send(Udp);
+    Udp.endPacket();
+    msg.empty();
 }
