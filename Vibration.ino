11dc21b27574238566e50ddb62873be46c3fbe61
InteractionEngineer <accounts@jona.email>
2024-04-12 19:49:22 +0200

    provided an example to control the vibration feedback - needs to be translated to a proper class

--- Vibration.ino
+++ Vibration.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+// Hardware: maybe disable LED
+// List of available vibration patterns https://learn.adafruit.com/assets/72593
+// complex waveforms possible by stringing together multiple effects in a row, up to 7. See https://cdn-shop.adafruit.com/datasheets/DRV2605.pdf
 
+#include <Wire.h>
+#include "Adafruit_DRV2605.h"
+
+Adafruit_DRV2605 drv;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DRV test");
+  drv.begin();
+    
+  // I2C trigger by sending 'go' command 
+  drv.setMode(DRV2605_MODE_INTTRIG); // default, internal trigger when sending GO command
+
+  drv.selectLibrary(1);
+  drv.setWaveform(0, 84);  // ramp up medium 1, see datasheet part 11.2
+  drv.setWaveform(1, 1);  // strong click 100%, see datasheet part 11.2
+  drv.setWaveform(2, 0);  // end of waveforms
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    drv.go();
+    delay(1000);
 }
