329e2c97c6c8f7af413475f0c59ced844a0926da
InteractionEngineer <accounts@jona.email>
2024-04-19 18:07:17 +0200

    erste Ideen für die Main

--- Schuetteldose.ino
+++ Schuetteldose.ino
@@ -1,11 +1,41 @@
-// hier sollen alle Prozesse koordiniert werden. @Davd wird noch eine Statemachine hierfür implementieren.
+// imports
+
+// controlled by OSC
+bool isFilled;
+bool isGrabbed;
+
+// controlled by loop
+long lastBatteryMillis;
+long lastPingMillis;
+
+// constants
+const int INTERVAL_BATTERY = 30*1000;
+const int INTERVAL_PING = 1*1000;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  isFilled = true;
+  isGrabbed = false;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // temporary variables
+  unsigned long currentMillis = millis();
+
+  // send battery
+  if (currentMillis - lastBatteryMillis > INTERVAL_BATTERY) {
+    // check battery
+    lastBatteryMillis = currentMillis;
+  }
+
+  // send ping
+  if (currentMillis - lastPingMillis > INTERVAL_PING) {
+    // check ping
+    lastPingMillis = currentMillis;
+  }
+
+  // provide feedback when spraying
+  if (isFilled && isGrabbed) {
+    // spray
+  }
 
 }
