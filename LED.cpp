51364624a13d1d5cd475e8fb5ce5dd8c51262937
InteractionEngineer <accounts@jona.email>
2024-08-24 16:42:48 +0200

    removed flashing capability

--- LED.cpp
+++ LED.cpp
@@ -5,18 +5,13 @@ LED::LED(int ledPin) {
   pinMode(_pin, OUTPUT);
 
   _state = IDLE;
-  // _flashCount = 0;
-  // _targetFlashCount = 0;
   _pulseCount = 0;
   _targetPulseCount = 0;
   _previousMillis = 0;
-  // _ledState = LOW;
-  // _flashInterval = 0;
   _pulseInterval = 0;
   _fadeAmount = 0;
   _brightness = 0;
 
-  // digitalWrite(_pin, _ledState);
   analogWrite(_pin, _brightness);
 }
 
@@ -37,19 +32,6 @@ void LED::pulse(int fadeAmount, int pulseInterval) {
   pulse(1, fadeAmount, pulseInterval);
 }
 
-// void LED::flash(int times, int duration_ms) {
-//   if (_state == IDLE) {
-//     _resetFlashAnimation(times, duration_ms);
-//     _state = FLASHING;
-//   }
-
-//   handleState();
-// }
-
-// void LED::flash(int duration_ms) {
-//   flash(1, duration_ms);
-// }
-
 void LED::_resetPulseAnimation(int times, int fadeAmount, int pulseInterval) {
   _targetPulseCount = times;
   _pulseCount = 0;
@@ -59,15 +41,6 @@ void LED::_resetPulseAnimation(int times, int fadeAmount, int pulseInterval) {
   _pulseInterval = pulseInterval;
   analogWrite(_pin, _brightness);
 }
-
-// void LED::_resetFlashAnimation(int times, int duration_ms) {
-//   _targetFlashCount = times;
-//   _flashCount = 0;
-//   _previousMillis = millis();
-//   _ledState = LOW;
-//   _flashInterval = duration_ms;
-//   digitalWrite(_pin, _ledState);
-// }
 
 void LED::handleState() {
   unsigned long currentMillis = millis();
@@ -82,16 +55,6 @@ void LED::handleState() {
         _state = TURNING_OFF;
       }
       break;
-
-      // case FLASHING:
-      //   if (_flashCount < _targetFlashCount * 2) {
-      //     if (currentMillis - _previousMillis >= _flashInterval) {
-      //       _actuallyFlash(currentMillis);
-      //     }
-      //   } else {
-      //     _state = TURNING_OFF;
-      //   }
-      //   break;
 
     case TURNING_OFF:
       turnOff();
@@ -108,13 +71,6 @@ void LED::handleState() {
   }
 }
 
-// void LED::_actuallyFlash(unsigned long currentMillis) {
-//   _previousMillis = currentMillis;
-//   _ledState = !_ledState;
-//   _flashCount++;
-//   digitalWrite(_pin, _ledState);
-// }
-
 void LED::_actuallyPulse(unsigned long currentMillis) {
   _previousMillis = currentMillis;
   _brightness += _fadeAmount;
@@ -130,7 +86,5 @@ void LED::_actuallyPulse(unsigned long currentMillis) {
 void LED::turnOff() {
   _brightness = 0;
   _targetPulseCount = 0;
-  // _targetFlashCount = 0;
-  // digitalWrite(_pin, LOW);
   analogWrite(_pin, _brightness);
 }
